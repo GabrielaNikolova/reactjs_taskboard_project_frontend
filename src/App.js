94bd3bc551764c19e19d16973c691f0ece82ec87
Gabriela Nikolova <62817091+GabrielaNikolova@users.noreply.github.com>
2023-04-05 16:02:41 +0300

    Created components directory
    Created Logo component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Logo from './components/Logo/Logo.js';
 
 function App() {
     return (
@@ -6,11 +7,7 @@ function App() {
             <div
                 className="flex flex-col w-screen h-screen overflow-auto text-gray-700 bg-gradient-to-tr from-blue-200 via-indigo-200 to-pink-200">
                 <div className="flex items-center flex-shrink-0 w-full h-16 px-10 bg-white bg-opacity-75">
-                    <svg className="w-8 h-8 text-indigo-600 stroke-current" xmlns="http://www.w3.org/2000/svg"
-                         fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                              d="M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01"/>
-                    </svg>
+                    <Logo/>
                     <input
                         className="flex items-center h-10 px-4 ml-10 text-sm bg-gray-200 rounded-full focus:outline-none focus:ring"
                         type="search" placeholder="Search for anything…"/>
---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,17 @@
+import './Logo.css';
+
+function Logo() {
+    return (
+        <svg className="logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 213.65 62.09">
+            <g id="Layer_2">
+                <g id="Layer_1-2">
+                    <text className="cls-1" transform="translate(91.22 45.15) scale(1.02 1.35)">BOARD</text>
+                    <text/>
+                    <text className="cls-2" transform="translate(0 45.15) scale(1.02 1.35)">TASK</text>
+                </g>
+            </g>
+        </svg>
+    );
+}
+
+export default Logo;
