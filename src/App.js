b5cea9031a589b12c312c9d6c46217f271ef2655
Gabriela Nikolova <62817091+GabrielaNikolova@users.noreply.github.com>
2023-04-06 17:19:40 +0300

    Components update
    Created Header and Navbar components

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
 import './App.css';
-import Logo from './components/Logo/Logo.js';
+import Header from './components/Header/Header.js';
 
 function App() {
     return (
         <div className="App">
             <div
                 className="flex flex-col w-screen h-screen overflow-auto text-gray-700 bg-gradient-to-tr from-blue-200 via-indigo-200 to-pink-200">
-                <div className="flex items-center flex-shrink-0 w-full h-16 px-10 bg-white bg-opacity-75">
-                    <Logo/>
-                    <input
-                        className="flex items-center h-10 px-4 ml-10 text-sm bg-gray-200 rounded-full focus:outline-none focus:ring"
-                        type="search" placeholder="Search for anything…"/>
-                    <div className="ml-10">
-                        <a className="mx-2 text-sm font-semibold text-indigo-700" href="#">Projects</a>
-                        <a className="mx-2 text-sm font-semibold text-gray-600 hover:text-indigo-700"
-                           href="#">Team</a>
-                        <a className="mx-2 text-sm font-semibold text-gray-600 hover:text-indigo-700"
-                           href="#">Activity</a>
-                    </div>
-                    <buton
-                        className="flex items-center justify-center w-8 h-8 ml-auto overflow-hidden rounded-full cursor-pointer">
-                        <img
-                            src="https://assets.codepen.io/5041378/internal/avatars/users/default.png?fit=crop&format=auto&height=512&version=1600304177&width=512"
-                            alt=""/>
-                    </buton>
-                </div>
+                <Header/>
                 <div className="px-10 mt-6">
                     <h1 className="text-2xl font-bold">Team Project Board</h1>
                 </div>
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,28 @@
+import Dropdown from 'react-bootstrap/Dropdown';
+import './Navbar.css';
+
+function Navbar() {
+    return (
+        <>
+            <div className="navigation">
+                <a className="mx-2 text-md font-semibold text-indigo-700" href="#">Projects</a>
+                <a className="mx-2 text-md font-semibold text-gray-600 hover:text-indigo-700" href="#">Team</a>
+            </div>
+            <div className="user-navigation">
+                <Dropdown>
+                    <Dropdown.Toggle className="toggle-button">Hello, User!</Dropdown.Toggle>
+                    <Dropdown.Menu className="dropdown-menu">
+                        <Dropdown.Item className="mx-2 text-md font-semibold text-indigo-700" href="#/action-1">Your Profile</Dropdown.Item>
+                        <Dropdown.Item className="mx-2 text-md font-semibold text-indigo-700" href="#/action-2">Settings</Dropdown.Item>
+                        <Dropdown.Item className="mx-2 text-md font-semibold text-indigo-700" href="#/action-3">Sign out</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+
+                <a type="button" href="#" className="nav-button mx-2 text-md font-semibold">Login</a>
+                <a type="button" href="#" className="nav-button mx-2 text-md font-semibold">Register</a>
+            </div>
+        </>
+    );
+}
+
+export default Navbar;
