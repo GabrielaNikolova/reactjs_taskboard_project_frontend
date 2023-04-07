62b9b9d3fb08027845fadb9371947a41d6af8219
Gabriela Nikolova <62817091+GabrielaNikolova@users.noreply.github.com>
2023-04-07 21:48:39 +0300

    Created new component
    Created Main component and updated CardsWrapper.css

--- src/components/CardsWrapper/CardsWrapper.js
+++ src/components/CardsWrapper/CardsWrapper.js
@@ -3,9 +3,9 @@ import Card from "../Card/Card";
 
 function CardsWrapper() {
     return (
-        <div className="card-wrapper">
-            <header className="card-wrapper-header">
-                <span className="card-wrapper-title">Backlog</span>
+        <div className="cards-wrapper">
+            <header className="cards-wrapper-header">
+                <span className="cards-wrapper-title">Backlog</span>
                 <span
                     className="cards-counter">6</span>
                 <button
@@ -23,13 +23,13 @@ function CardsWrapper() {
             <Card/>
             <Card/>
             </div>
-            <footer className="card-wrapper-footer">
+            <footer className="cards-wrapper-footer">
                 <a className="add-card-composer text-sm font-semibold" href="#">
                     <svg className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                               d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
                     </svg>
-                    <span className="js-add-a-card">Add a card</span>
+                    <span className="add-a-card">Add a card</span>
                 </a>
             </footer>
         </div>
