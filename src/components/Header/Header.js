83fe526c3eeee4f129cef3b6230eab43366094c4
Gabriela Nikolova <62817091+GabrielaNikolova@users.noreply.github.com>
2023-04-14 06:13:06 +0300

    Updated components
    Updated Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,46 @@
 import './Header.css';
 import Logo from "./Logo/Logo";
-import Navbar from "./Navbar/Navbar";
+import Dropdown from "react-bootstrap/Dropdown";
 
 function Header() {
     return (
         <header className="header">
             <Logo/>
-            <Navbar/>
+            <div className="navigation">
+                <Dropdown>
+                    <Dropdown.Toggle className="toggle-button">Projects</Dropdown.Toggle>
+                    <Dropdown.Menu className="dropdown-menu">
+                        <Dropdown.Item className="mx-2 text-md font-semibold text-indigo-700" href="#/action-1">All
+                            Projects</Dropdown.Item>
+
+                        <Dropdown.Item
+                                       className="mx-2 text-md font-semibold text-indigo-700" href="#/action-2">Add New
+                            Project</Dropdown.Item>
+
+                    </Dropdown.Menu>
+                </Dropdown>
+                <a className="mx-2 text-md font-semibold text-gray-600 hover:text-indigo-700" href="#">Team</a>
+            </div>
+            <div className="user-navigation">
+                <Dropdown>
+                    <Dropdown.Toggle className="toggle-button">Hello, User!</Dropdown.Toggle>
+                    <Dropdown.Menu className="dropdown-menu">
+                        <Dropdown.Item className="mx-2 text-md font-semibold text-indigo-700" href="#/action-1">Your
+                            Profile</Dropdown.Item>
+                        <Dropdown.Item className="mx-2 text-md font-semibold text-indigo-700" href="#/action-3">Sign
+                            out</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+                <>
+                    <a  type="button" href="#"
+                        className="nav-button mx-2 text-md font-semibold">Login</a>
+
+                    <a type="button" href="#"
+                       className="nav-button text-md font-semibold">Register</a>
+                    
+
+                </>
+            </div>
         </header>
 
     );
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -6,7 +6,6 @@ import Button from "../Button/Button";
 function Login() {
 
     return (
-
             <div
                 className="login-page w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0">
                 <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
