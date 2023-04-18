d849e464e4a94439e29be5132d8169291b29aa46
Gabriela Nikolova <62817091+GabrielaNikolova@users.noreply.github.com>
2023-04-18 18:10:03 +0300

    Component update
    Updated TaskDetails component

--- src/components/TaskDetails/TaskDetails.js
+++ src/components/TaskDetails/TaskDetails.js
@@ -2,7 +2,7 @@ import {Modal} from "react-bootstrap";
 import './TaskDetails.css';
 import Button from "../Button/Button";
 import CommentsSection from "../CommentsSection/CommentsSection";
-import {useNavigate, useParams} from "react-router-dom";
+import {Link, useNavigate, useParams} from "react-router-dom";
 import {useContext, useEffect} from "react";
 import {TaskContext} from "../../contexts/TaskContext";
 import {useAuthentication} from "../../contexts/AuthContext";
@@ -10,7 +10,7 @@ import {useAuthentication} from "../../contexts/AuthContext";
 import * as commentService from '../../services/commentService';
 import * as taskService from '../../services/taskService';
 
-function TaskDetails(props) {
+function TaskDetails() {
 
     const navigate = useNavigate();
     const {addComment, getTaskDetails, selectTask, deleteTask} = useContext(TaskContext);
@@ -55,49 +55,52 @@ function TaskDetails(props) {
 
 
     return (
-        <Modal
-            show={props.show}
-            cancel={props.close}
-            size="md"
-            centered
-            className={"details-box"}
-        >
+        // <Modal
+        //     show={props.show}
+        //     cancel={props.close}
+        //     size="md"
+        //     centered
+        //     className={"details-box"}
+        // >
 
-            <div className="relative p-4 bg-white rounded-lg shadow">
+        <div className="details-box relative p-4 bg-white rounded-lg shadow">
 
-                <div
-                    className="flex justify-between items-center mb-4 rounded-t border-b sm:mb-5">
-                    <h3 className="text-gray-900 font-medium text-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-1">Title
-                    </h3>
-                    <Button value={"Edit"}/>
-                    <button type="button"
-                            className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center">
-                        <span className="material-icons">close</span>
-                    </button>
-                </div>
-                <div className="relative">
-                    <label htmlFor="description"
-                           className="block mb-2 text-sm font-medium text-gray-900">Description</label>
-                    <p className="desc-box">Standard glass, 3.8GHz 8-core 10th-generation Intel Core i7
-                        processor, Turbo Boost up to 5.0GHz, 16GB 2666MHz DDR4 memory, Radeon Pro 5500 XT with 8GB of
-                        GDDR6 memory, 256GB SSD storage, Gigabit Ethernet, Magic Mouse 2, Magic Keyboard - US</p>
-                </div>
-                <div className="flex justify-center mb-4 mt-3">
-                    <div className="relative mr-10">
-                        <label htmlFor="priority"
-                               className="block mb-2 text-decoration-underline text-sm font-medium text-gray-900">Priority:</label>
-                        <span><p className="mb-2 text-sm font-medium">Low priority</p></span>
-                    </div>
-                    <div className="relative mr-4">
-                        <label htmlFor="currentCategory"
-                               className="block mb-2 text-decoration-underline text-sm font-medium text-gray-900">Category:</label>
-                        <span><p className="mb-2 text-sm font-medium">In test</p></span>
-                    </div>
-                </div>
-                <CommentsSection/>
+            <div
+                className="flex justify-between items-center mb-4 rounded-t border-b sm:mb-5">
+                <h3 className="text-gray-900 font-medium text-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-1">{currentTask.title}</h3>
+                <Link to='/tasks' type="button"
+                      className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center">
+                    <span className="material-icons">close</span>
+                </Link>
             </div>
+            <div className="relative">
+                <label htmlFor="description"
+                       className="block mb-2 text-sm font-medium text-gray-900">Description</label>
+                <p className="desc-box">{currentTask.description}</p>
+            </div>
+            <div className="flex justify-center mb-4 mt-3">
+                <div className="relative mr-10">
+                    <label htmlFor="priority"
+                           className="block mb-2 text-decoration-underline text-sm font-medium text-gray-900">Priority:</label>
+                    <span><p className="mb-2 text-sm font-medium">{currentTask.priority}</p></span>
+                </div>
+                <div className="relative mr-4">
+                    <label htmlFor="currentCategory"
+                           className="block mb-2 text-decoration-underline text-sm font-medium text-gray-900">Category:</label>
+                    <span><p className="mb-2 text-sm font-medium">{currentTask.category}</p></span>
+                </div>
+            </div>
+            <div className="mb-2">
+                <Link to={`/tasks/${taskId}/edit`} className="mr-2">
+                    <Button value={"Edit"}/>
+                </Link>
+                <Button className="delete-card-btn hover:text-white hover:bg-red-600" onClick={taskDeleteHandler} value={"Delete"}/>
+            </div>
+            <CommentsSection/>
+        </div>
 
-        </Modal>
+
+        // </Modal>
 
     );
 }
